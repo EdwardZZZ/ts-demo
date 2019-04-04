aac9f1c22dc31e896198fdc9104ba81673ee6185
大华 <zzh627@126.com>
2019-04-04 10:48:50 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process');
 require('ts-mocha');
 const Mocha = require('mocha');
 const mocha = new Mocha();
@@ -13,12 +14,78 @@ const fullPath = (p) => path.resolve(ROOT, p);
  * addFile eg: mocha.addFile('./test/tcp.spec.ts'));
  */
 process.env.MOCHAMODE = 'multy';
-fs.readdirSync(fullPath('test')).forEach((name) => {
+fs.readdirSync(fullPath('test/spec')).forEach((name) => {
     if (name.slice(-7) === 'spec.ts') {
-        mocha.addFile(fullPath(`test/${name}`));
+        mocha.addFile(fullPath(`test/spec/${name}`));
     }
 });
 
-mocha.run(() => {
-    process.exit();
-});
+function testV3(cb) {
+    const m = child_process.spawn('mocha', [
+        'test/index',
+        'v3',
+    ], {
+        cwd: process.cwd(),
+    });
+
+    m.stdout.on('data', (data) => {
+        const logV3 = data.toString().replace(/\n$/, '');
+        console.log(logV3);
+        if (logV3.indexOf('v3 err') > -1) {
+            cb( new Error('v3 err'));
+        }
+
+        if (logV3.indexOf('v3 end') > -1) {
+            cb();
+        }
+    });
+}
+
+function testV4(cb) {
+    const m = child_process.spawn('mocha', [
+        'test/index',
+        'v4',
+    ], {
+        cwd: process.cwd(),
+    });
+
+    m.stdout.on('data', (data) => {
+        const logV3 = data.toString().replace(/\n$/, '');
+        console.log(logV3);
+        if (logV3.indexOf('v4 err') > -1) {
+            cb( new Error('v4 err'));
+        }
+
+        if (logV3.indexOf('v4 end') > -1) {
+            cb();
+        }
+    });
+}
+
+if (process.argv.indexOf('v4') > -1) {
+    mocha.run(async (err) => {
+        console.log(err ? 'v4 err' : 'v4 end');
+        process.exit();
+    });
+} else if (process.argv.indexOf('v3') > -1) {
+    mocha.run(async (err) => {
+        console.log(err ? 'v3 err' : 'v3 end');
+        process.exit();
+    });
+} else if (process.argv.indexOf('all') > -1) {
+    testV4((errV4) => {
+        if (errV4) process.exit();
+
+        testV3((errV3) => {
+            if (errV3) process.exit();
+    
+            mocha.run(async () => {
+                process.exit();
+            });
+        });
+    });
+} else {
+    mocha.run(async () => {
+        process.exit();
+    });
+}
